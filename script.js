0a8f223104755e6d8c4b94e2d8ad0cfdced0b789
Safin Rahman <hrsafin2434@gmail.com>
2023-04-30 22:24:37 +0600

    weather app using free API

---
+++ script.js
@@ -0,0 +1,47 @@
+
+const options = {
+    method: 'GET',
+    headers: {
+        'content-type': 'application/octet-stream',
+        'X-RapidAPI-Key': '5572293d5bmsh31bcb4dd9338656p1dd05bjsn5c76d90cee95',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+const getData = (city) => {
+    cityName.innerHTML = city
+    
+    
+    
+    
+        fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response);
+
+            cloud_pct.innerHTML = response.cloud_pct ,
+            temp.innerHTML = response.temp,
+            feels_like.innerHTML = response.feels_like,
+            humidity.innerHTML = response.humidity,
+            min_temp.innerHTML = response.min_temp,
+            max_temp.innerHTML = response.max_temp,
+            wind_speed.innerHTML = response.wind_speed,
+            // wind_degrees.innerHTML = result.wind_degrees,
+            sunrise.innerHTML = response.sunrise,
+            sunset.innerHTML = response.sunset
+        }).catch((error) =>{
+            console.error(error);
+         }) 
+        
+
+       
+     
+}
+
+
+submit.addEventListener("click", (e) =>{
+    e.preventDefault() 
+   getData(city.value)
+})
+
+getData("Dhaka")
+
